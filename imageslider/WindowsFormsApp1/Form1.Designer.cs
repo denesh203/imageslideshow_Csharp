f84ffdeb4c13e6f0787bbc3fa02e901796a11f40
Denesh neupane <ecedenesh.203@gmail.com>
2024-02-27 17:13:52 +0900

    Add files via upload

---
+++ imageslider/WindowsFormsApp1/Form1.Designer.cs
@@ -0,0 +1,215 @@
+﻿namespace WindowsFormsApp1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.panel4 = new System.Windows.Forms.Panel();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.homeToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.aboutUsToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.helpToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.exitToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.folderBrowserDialog1 = new System.Windows.Forms.FolderBrowserDialog();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.Exit = new System.Windows.Forms.Button();
+            this.Next = new System.Windows.Forms.Button();
+            this.Play_Pause = new System.Windows.Forms.Button();
+            this.Preview = new System.Windows.Forms.Button();
+            this.OpenFolder = new System.Windows.Forms.Button();
+            this.panel1.SuspendLayout();
+            this.panel4.SuspendLayout();
+            this.menuStrip1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.panel1.Controls.Add(this.OpenFolder);
+            this.panel1.Controls.Add(this.Exit);
+            this.panel1.Controls.Add(this.panel4);
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.panel1.Location = new System.Drawing.Point(0, 567);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(1197, 71);
+            this.panel1.TabIndex = 2;
+            // 
+            // panel4
+            // 
+            this.panel4.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom)));
+            this.panel4.Controls.Add(this.Next);
+            this.panel4.Controls.Add(this.Play_Pause);
+            this.panel4.Controls.Add(this.Preview);
+            this.panel4.Location = new System.Drawing.Point(380, -1);
+            this.panel4.Name = "panel4";
+            this.panel4.Size = new System.Drawing.Size(330, 71);
+            this.panel4.TabIndex = 1;
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.homeToolStripMenuItem,
+            this.aboutUsToolStripMenuItem,
+            this.helpToolStripMenuItem,
+            this.exitToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(1197, 24);
+            this.menuStrip1.TabIndex = 4;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // homeToolStripMenuItem
+            // 
+            this.homeToolStripMenuItem.Name = "homeToolStripMenuItem";
+            this.homeToolStripMenuItem.Size = new System.Drawing.Size(52, 20);
+            this.homeToolStripMenuItem.Text = "Home";
+            // 
+            // aboutUsToolStripMenuItem
+            // 
+            this.aboutUsToolStripMenuItem.Name = "aboutUsToolStripMenuItem";
+            this.aboutUsToolStripMenuItem.Size = new System.Drawing.Size(67, 20);
+            this.aboutUsToolStripMenuItem.Text = "About us";
+            // 
+            // helpToolStripMenuItem
+            // 
+            this.helpToolStripMenuItem.Name = "helpToolStripMenuItem";
+            this.helpToolStripMenuItem.Size = new System.Drawing.Size(44, 20);
+            this.helpToolStripMenuItem.Text = "Help";
+            // 
+            // exitToolStripMenuItem
+            // 
+            this.exitToolStripMenuItem.Name = "exitToolStripMenuItem";
+            this.exitToolStripMenuItem.Size = new System.Drawing.Size(38, 20);
+            this.exitToolStripMenuItem.Text = "Exit";
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.pictureBox1.Location = new System.Drawing.Point(0, 24);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(1197, 543);
+            this.pictureBox1.TabIndex = 3;
+            this.pictureBox1.TabStop = false;
+            // 
+            // Exit
+            // 
+            this.Exit.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.Exit.Font = new System.Drawing.Font("MS UI Gothic", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            this.Exit.Location = new System.Drawing.Point(1086, 16);
+            this.Exit.Name = "Exit";
+            this.Exit.Size = new System.Drawing.Size(98, 42);
+            this.Exit.TabIndex = 1;
+            this.Exit.Text = "STOP";
+            this.Exit.UseVisualStyleBackColor = true;
+            this.Exit.Click += new System.EventHandler(this.Exit_Click);
+            // 
+            // Next
+            // 
+            this.Next.Image = global::WindowsFormsApp1.Properties.Resources.next_track_button_emoji;
+            this.Next.Location = new System.Drawing.Point(226, 0);
+            this.Next.Name = "Next";
+            this.Next.Size = new System.Drawing.Size(93, 71);
+            this.Next.TabIndex = 3;
+            this.Next.UseVisualStyleBackColor = true;
+            this.Next.Click += new System.EventHandler(this.Next_Click);
+            // 
+            // Play_Pause
+            // 
+            this.Play_Pause.Image = global::WindowsFormsApp1.Properties.Resources.pp;
+            this.Play_Pause.Location = new System.Drawing.Point(127, 0);
+            this.Play_Pause.Name = "Play_Pause";
+            this.Play_Pause.Size = new System.Drawing.Size(93, 71);
+            this.Play_Pause.TabIndex = 2;
+            this.Play_Pause.UseVisualStyleBackColor = true;
+            this.Play_Pause.Click += new System.EventHandler(this.Play_Pause_Click);
+            // 
+            // Preview
+            // 
+            this.Preview.Image = global::WindowsFormsApp1.Properties.Resources.Capture11;
+            this.Preview.Location = new System.Drawing.Point(28, 0);
+            this.Preview.Name = "Preview";
+            this.Preview.Size = new System.Drawing.Size(93, 71);
+            this.Preview.TabIndex = 1;
+            this.Preview.UseVisualStyleBackColor = true;
+            this.Preview.Click += new System.EventHandler(this.Preview_Click);
+            // 
+            // OpenFolder
+            // 
+            this.OpenFolder.Font = new System.Drawing.Font("MS UI Gothic", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            this.OpenFolder.Location = new System.Drawing.Point(11, 16);
+            this.OpenFolder.Name = "OpenFolder";
+            this.OpenFolder.Size = new System.Drawing.Size(98, 42);
+            this.OpenFolder.TabIndex = 0;
+            this.OpenFolder.Text = "OPEN";
+            this.OpenFolder.UseVisualStyleBackColor = true;
+            this.OpenFolder.Click += new System.EventHandler(this.OpenFolder_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1197, 638);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.panel1.ResumeLayout(false);
+            this.panel4.ResumeLayout(false);
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem homeToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem aboutUsToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem helpToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem exitToolStripMenuItem;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Panel panel4;
+        private System.Windows.Forms.Button Next;
+        private System.Windows.Forms.Button Play_Pause;
+        private System.Windows.Forms.Button Preview;
+        private System.Windows.Forms.Button Exit;
+        private System.Windows.Forms.Button OpenFolder;
+        private System.Windows.Forms.FolderBrowserDialog folderBrowserDialog1;
+    }
+}
+
---
+++ imageslider/WindowsFormsApp1/Form1.cs
@@ -0,0 +1,168 @@
+﻿using System;
+using System.Drawing;
+using System.IO;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp1
+{
+    public partial class Form1 : Form
+    {
+         // Define a CircularButton variable
+        private string[] folderFile = null;
+        private int selected = 0;
+        private int begin = 0;
+        private int end = 0;
+
+        public Form1()
+        {
+            InitializeComponent();
+
+            
+        }
+
+        private void Button1_Click(object sender, EventArgs e)
+        {
+            // Handle click event for button1 (Pause button)
+        }
+
+      
+
+        private void OpenFolder_Click(object sender, EventArgs e)
+        {
+            if (folderBrowserDialog1.ShowDialog() == DialogResult.OK)
+            {
+                string[] part1 = null, part2 = null, part3 = null;
+
+                part1 = Directory.GetFiles(folderBrowserDialog1.SelectedPath, "*.jpg");
+                part2 = Directory.GetFiles(folderBrowserDialog1.SelectedPath, "*.jpeg");
+                part3 = Directory.GetFiles(folderBrowserDialog1.SelectedPath, "*.bmp");
+
+                folderFile = new string[part1.Length + part2.Length + part3.Length];
+
+                Array.Copy(part1, 0, folderFile, 0, part1.Length);
+                Array.Copy(part2, 0, folderFile, part1.Length, part2.Length);
+                Array.Copy(part3, 0, folderFile, part1.Length + part2.Length, part3.Length);
+
+                selected = 0;
+                begin = 0;
+                end = folderFile.Length;
+
+                showImage(folderFile[selected]);
+
+
+            }
+        }
+        private void showImage(string path)
+        {
+
+            Image imgtemp = Image.FromFile(path);
+
+            // Calculate the aspect ratio
+            double aspectRatioImg = (double)imgtemp.Width / imgtemp.Height;
+            double aspectRatioPictureBox = (double)pictureBox1.Width / pictureBox1.Height;
+
+            int newWidth;
+            int newHeight;
+
+            if (aspectRatioImg > aspectRatioPictureBox)
+            {
+                // Image is wider than PictureBox
+                newWidth = pictureBox1.Width;
+                newHeight = (int)(newWidth / aspectRatioImg);
+            }
+            else
+            {
+                // Image is taller than PictureBox
+                newHeight = pictureBox1.Height;
+                newWidth = (int)(newHeight * aspectRatioImg);
+            }
+
+            // Resize the image
+            Image resizedImage = new Bitmap(imgtemp, newWidth, newHeight);
+
+            // Draw the resized image onto a new bitmap that fills the PictureBox
+            Bitmap filledImage = new Bitmap(pictureBox1.Width, pictureBox1.Height);
+            using (Graphics g = Graphics.FromImage(filledImage))
+            {
+                g.FillRectangle(Brushes.Black, 0, 0, pictureBox1.Width, pictureBox1.Height);
+                int x = (pictureBox1.Width - newWidth) / 2;
+                int y = (pictureBox1.Height - newHeight) / 2;
+                g.DrawImage(resizedImage, x, y, newWidth, newHeight);
+            }
+
+            // Assign the filled image to pictureBox1
+            pictureBox1.Image = filledImage;
+        }
+
+        private void prevImage()
+        {
+            if (selected == 0)
+            {
+                selected = folderFile.Length - 1;
+                showImage(folderFile[selected]);
+            }
+            else
+            {
+                selected = selected - 1;
+                showImage(folderFile[selected]);
+            }
+        }
+
+        private void nextImage()
+        {
+            if (selected == folderFile.Length - 1)
+            {
+                selected = 0;
+                showImage(folderFile[selected]);
+            }
+            else
+            {
+                selected = selected + 1;
+                showImage(folderFile[selected]);
+            }
+        }
+
+        private void Preview_Click(object sender, EventArgs e)
+        {
+            prevImage();
+        }
+
+        private void Next_Click(object sender, EventArgs e)
+        {
+            nextImage();
+        }
+        private void timer1_Tick(object sender, System.EventArgs e)
+        {
+            nextImage();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            
+            this.timer1.Interval = 1000;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+
+        }
+
+        private void Play_Pause_Click(object sender, EventArgs e)
+        {
+            string imagePath = @"D:\arduino-nightly-windows\play.PNG";
+            string imagePath1 = @"D:\arduino-nightly-windows\pause.PNG";
+            if (timer1.Enabled == true)
+            {
+                timer1.Enabled = false;
+                Play_Pause.Image = Image.FromFile(imagePath);
+            }
+            else
+            {
+                timer1.Enabled = true;
+                Play_Pause.Image = Image.FromFile(imagePath1);
+            }
+        }
+
+        private void Exit_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+    }
+}
